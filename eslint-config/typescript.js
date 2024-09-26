11c1246ec94dc4805644bcfc5ca61bedafdbab17
Sebastian Noack <sebastian@reviewable.com>
2024-09-25 20:37:12 -0400

    Add shared ESLint configuration and tsconfig

---
+++ eslint-config/typescript.js
@@ -0,0 +1,33 @@
+import typescript from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  ...typescript.configs.recommended,
+  importPlugin.flatConfigs.typescript,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', {args: 'none', varsIgnorePattern: '^unused'}],
+
+      // https://stackoverflow.com/a/63961972
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define':
+        ['error', {functions: false, ignoreTypeReferences: true}],
+
+      // Rules turned off for now
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off'
+    }
+  }
+];
